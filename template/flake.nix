ac94890daf95534c07ed98c638d63a0678eea5fc
k1nkreet <polyakovskiy.ilya@gmail.com>
2022-02-04 15:04:15 +0300

    add kernels

--- template/flake.nix
+++ template/flake.nix
@@ -17,13 +17,30 @@
     let
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          jupyterWith.overlays.jupyterWith
+        overlays = builtins.attrValues jupyterWith.overlays;
+      };
+
+      iPython = pkgs.jupyterWith.kernels.iPythonWith {
+        name = "Python-data-env";
+        ignoreCollisions = true;
+        packages = p: [
+          p.numpy
         ];
       };
+
+      iHaskell = pkgs.jupyterWith.kernels.iHaskellWith {
+        name = "ihaskell-flake";
+        packages = p: with p; [ vector aeson ];
+        extraIHaskellFlags = "--codemirror Haskell"; # for jupyterlab syntax highlighting
+        haskellPackages = pkgs.haskellPackages;
+      };
+
       jupyterEnvironment =
         pkgs.jupyterWith.jupyterlabWith {
-          kernels = [];
+          kernels = [
+            iPython
+            iHaskell
+          ];
         };
     in rec {
       defaultPackage = packages.jupyterEnvironment;
