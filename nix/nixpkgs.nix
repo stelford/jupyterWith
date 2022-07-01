8e3ea8b6f6886ea62f6e44b616ec800804d2ff37
stelford <stef@ummon.com>
2022-07-01 11:22:01 -0400

    Update nixpkgs.nix
    Signed-off-by: stelford <stef@ummon.com>

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,6 +1,1 @@
-let
-
-  nixpkgs = fetchTarball "https://api.${nixpkgsLocked.host or "github.com"}/repos/${nixpkgsLocked.owner}/${nixpkgsLocked.repo}/tarball/${nixpkgsLocked.rev}";
-
-in
-  import nixpkgs
+import <nixpkgs> {};
